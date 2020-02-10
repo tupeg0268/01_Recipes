c28527fec3b98caecb62a5b64e4df0e8da593286
tupeg0268 <60673066+tupeg0268@users.noreply.github.com>
2020-02-11 11:14:26 +1300

    Recipe gets name and checks it

---
+++ 00_sandpit.py
@@ -0,0 +1,11 @@
+# function goes here
+def count(low, high):
+    for item in range(low, high+1):
+        print(item)
+
+# main routine goes here
+for item in range(1,10+1):
+    print(item)
+
+for item in range(25,35+1):
+    print(item)
---
+++ 01 _recipe _name.py
@@ -0,0 +1,20 @@
+# Get's recipe name and checks it is not blank
+
+# Not Blank Function goes here
+def not_blank(question) :
+    print("hello")
+    valid = False
+    while not valid:
+        response = input(question)
+
+        if response == "":
+            continue
+        else:
+           return response
+
+
+# Main Routine goes here
+
+recipe_name = not_blank( "what is the name? ")
+
+print("you are making {}".format(recipe_name))
